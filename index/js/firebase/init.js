197b2a4e3c5b251980a79b61e91fd0a8dd5c381a
valleysameday <valleysameday@gmail.com>
2025-12-21 01:57:32 +0000

    Update init.js

--- index/js/firebase/init.js
+++ index/js/firebase/init.js
@@ -1,20 +1,22 @@
+// init.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 
-let auth, db, storage;
+export async function getFirebase() {
+  try {
+    const res = await fetch('/.netlify/functions/firebaseConfig');
+    const firebaseConfig = await res.json();
 
-async function initFirebase() {
-  const res = await fetch('/.netlify/functions/firebaseConfig');
-  const firebaseConfig = await res.json();
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    const db = getFirestore(app);
+    const storage = getStorage(app);
 
-  const app = initializeApp(firebaseConfig);
-  auth = getAuth(app);
-  db = getFirestore(app);
-  storage = getStorage(app);
+    return { auth, db, storage };
+  } catch (err) {
+    console.error("Firebase init failed:", err);
+    throw err;
+  }
 }
-
-await initFirebase();
-
-export { auth, db, storage };
