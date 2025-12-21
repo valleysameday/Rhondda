364e1ac9c5a885e53fb24b0a794bd8f9bd116659
valleysameday <valleysameday@gmail.com>
2025-12-21 01:50:54 +0000

    Update init.js

--- index/js/firebase/init.js
+++ index/js/firebase/init.js
@@ -3,14 +3,18 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 
+let auth, db, storage;
+
 async function initFirebase() {
   const res = await fetch('/.netlify/functions/firebaseConfig');
   const firebaseConfig = await res.json();
 
   const app = initializeApp(firebaseConfig);
-  window.auth = getAuth(app);
-  window.db = getFirestore(app);
-  window.storage = getStorage(app);
+  auth = getAuth(app);
+  db = getFirestore(app);
+  storage = getStorage(app);
 }
 
-initFirebase();
+await initFirebase();
+
+export { auth, db, storage };
