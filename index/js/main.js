058220256e01a99ce46bdf7d6f9b5e910430d1f0
valleysameday <valleysameday@gmail.com>
2025-12-21 02:01:21 +0000

    Update main.js

--- index/js/main.js
+++ index/js/main.js
@@ -1,7 +1,7 @@
 import { auth, db, storage } from '/index/js/firebase/init.js';
 import { initUIRouter } from '/index/js/ui-router.js';
 import { initFeed } from '/index/js/feed.js';
-import './post-gate.js'; // post-gate already imports firebase, no need to call init
+import 'index/js/post-gate.js'; // post-gate already imports firebase, no need to call init
 
 document.addEventListener('DOMContentLoaded', () => {
   initUIRouter();
