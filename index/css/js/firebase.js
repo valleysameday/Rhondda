f756a2e141587c7656731b9b69c69a66346c4096
valleysameday <valleysameday@gmail.com>
2025-12-23 22:57:12 +0000

    Update firebase.js

--- index/css/js/firebase.js
+++ index/css/js/firebase.js
@@ -1,4 +1,4 @@
-// /index/js/firebase.js
+/// /index/js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
