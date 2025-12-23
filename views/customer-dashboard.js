dde78eb8bf25f96257528453e966bb81d44dd1b5
valleysameday <valleysameday@gmail.com>
2025-12-23 06:34:58 +0000

    Update customer-dashboard.js

--- views/customer-dashboard.js
+++ views/customer-dashboard.js
@@ -1,5 +1,5 @@
 // /index/js/dashboard-customer.js
-import { initFeed } from "./feed.js";
+import { initFeed } from "/js/feed.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initFeed();
