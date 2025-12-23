1d1c54f5f114348b29c3dc67021d79f6d2c50ca1
valleysameday <valleysameday@gmail.com>
2025-12-23 09:31:40 +0000

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -10,13 +10,12 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-s
 // process.env.YOUR_VAR_NAME is how Netlify exposes env vars in builds
 // --------------------------
 const firebaseConfig = {
-  apiKey: window.FIREBASE_API_KEY,
-  authDomain: window.FIREBASE_AUTH_DOMAIN,
-  projectId: window.FIREBASE_PROJECT_ID,
-  storageBucket: window.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: window.FIREBASE_MESSAGING_SENDER_ID,
-  appId: window.FIREBASE_APP_ID,
-  measurementId: window.FIREBASE_MEASUREMENT_ID
+  apiKey: window.PUBLIC_FIREBASE_API_KEY,
+  authDomain: window.PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: window.PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: window.PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: window.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: window.PUBLIC_FIREBASE_APP_ID
 };
 
 // --------------------------
