d1998afae6b45fbb690b7d1d79b86699e3f1b519
valleysameday <valleysameday@gmail.com>
2025-12-23 15:40:03 +0000

    Create smartbar.js

---
+++ src/js/smartbar.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Smart Bar ready");
+
+  // Future:
+  // - Load weather
+  // - Load trending tags
+  // - Load AI suggestions
+  // - Load categories dynamically
+  // - Load location
+});
