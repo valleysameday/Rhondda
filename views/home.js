2e6483d3add8cc4ad5b417190c6a9e11e3e562af
valleysameday <valleysameday@gmail.com>
2025-12-23 07:10:52 +0000

    Update home.js

--- views/home.js
+++ views/home.js
@@ -1,6 +1,6 @@
+// /views/home.js
 import { initFeed } from '/index/js/feed.js';
 
-window.addEventListener("DOMContentLoaded", () => {
-  const feedContainer = document.getElementById("feed");
-  if (feedContainer) initFeed();
-});
+export function init() {
+  initFeed(); // now called after HTML is in DOM
+}
