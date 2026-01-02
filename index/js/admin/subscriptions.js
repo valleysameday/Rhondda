5962ae5727e7ab1040bc7f9dfbec138a7f5722d4
valleysameday <valleysameday@gmail.com>
2026-01-02 18:02:53 +0000

    Update subscriptions.js

--- index/js/admin/subscriptions.js
+++ index/js/admin/subscriptions.js
@@ -1,6 +1,6 @@
 // index/js/admin/subscriptions.js
 import { collection, doc, getDocs, query, where, updateDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { showToast } from "./utils.js";
+import { showToast } from "/index/js/admin/utils.js";
 
 let db;
 
