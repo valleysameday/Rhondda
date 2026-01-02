260672363ccbff1f6ec0cb52a83289846b15c6ba
valleysameday <valleysameday@gmail.com>
2026-01-02 18:01:42 +0000

    Update overview.js

--- index/js/admin/overview.js
+++ index/js/admin/overview.js
@@ -1,6 +1,6 @@
 // index/js/admin/overview.js
 import { collection, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { showToast } from "./utils.js";
+import { showToast } from "/index/js/admin/utils.js";
 
 let db;
 
