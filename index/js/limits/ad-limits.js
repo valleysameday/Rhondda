3f56ba5349803a996e6cf5432ff2346bee0e88ed
valleysameday <valleysameday@gmail.com>
2026-01-03 13:10:22 +0000

    Create ad-limits.js

---
+++ index/js/limits/ad-limits.js
@@ -0,0 +1,15 @@
+// index/js/limits/ad-limits.js
+import { AI } from "../ai/assistant.js";
+
+export function checkAdLimit(adCount, user) {
+  if (adCount === 3) {
+    AI.speak("AD_LIMIT_WARNING", { name: user.name });
+  }
+
+  if (adCount >= 4) {
+    AI.speak("AD_LIMIT_REACHED", { name: user.name });
+    return false;
+  }
+
+  return true;
+}
